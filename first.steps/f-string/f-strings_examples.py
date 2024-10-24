4211ed955459875e23105c1712969cbabaafd340
LucaGiona <luca@senzproduction.com>
2024-10-24 15:29:36 +0200

    f-string examples

---
+++ first.steps/f-string/f-strings_examples.py
@@ -0,0 +1,13 @@
+names = ["Steve", "John", "Katherine", "Paul"]
+
+for name in names:
+    print(f"{name:15} centre {name:>15}")
+    #seoicfing white space between name
+
+
+name = 'Larry'
+age = 34
+city = "New York"
+greeting = f"Hi {name}, you are {age} years of age"
+
+print(greeting + f", and you live in {city}")
