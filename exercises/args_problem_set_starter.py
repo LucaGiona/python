6a1056e75e80d3b295d7bfd29cbe1030b6d0ff70
LucaGiona <luca@senzproduction.com>
2024-10-30 16:30:02 +0100

    exer args and kwargs

--- exercises/args_problem_set_starter.py
+++ exercises/args_problem_set_starter.py
@@ -3,6 +3,14 @@
 # The function should return True if it is passed "pickle" as one of the args
 # Otherwise it should return False
 
+def contains_pickle(*args):
+    for arg in args:
+        if arg == "pickle":
+            return True
+
+print(contains_pickle("red", 45, "pickle", []))
+print(contains_pickle(1,2, "blue"))
+
 #  contains_pickle("red", 45, "pickle", [])  --> True
 #  contains_pickle(1,2, "blue") ---------------> False
 
@@ -16,6 +24,17 @@
 # It should accept any number of arguments
 # It should return a count of how many args are less than or equal to 50
 
+def count_fails(*args):
+    count = 0
+    for arg in args:   
+        if arg <= 50:
+            count +=1
+    return count
+
+print(count_fails(99,48,79,36))
+print(count_fails(85,78,91))
+print(count_fails(50,41,47,74,76,81))
+
 # count_fails(99,48,79,36) -------> 2
 # count_fails(85,78,91) ----------> 0
 # count_fails(50,41,47,74,76,81) -> 3
@@ -28,6 +47,15 @@
 # Write a function called get_top_students that helps teachers find their A-grade students!
 # It should accept any number of student=score keyword arguments like colt=78 or elton=98
 # It should return a list containing the names of students who scored 90 or above
+
+def get_top_students(**kwargs):
+    top_students = []
+    for name, score in kwargs.items():
+        if score >= 90: 
+            top_students.append(name)
+    return top_students
+
+print(get_top_students(tim=91, stacy=83, carlos=97, jim=69))
 
 # get_top_students(tim=91, stacy=83, carlos=97, jim=69) -> ['tim', 'carlos']
 # get_top_students(colt=61, elton=76) -------------------> []
