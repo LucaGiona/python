6e13e5b94f1ee6468f2e699029faf695445c260f
LucaGiona <luca@senzproduction.com>
2024-11-06 02:03:39 +0100

    exercises for loop in loop + dict intro

---
+++ first.steps/cs50/Loops/potter2.py
@@ -0,0 +1,11 @@
+students = [
+    {"name":"Hermione", "house": "Gryffindor", "patronus": "Otter"}, 
+    {"name":"Harry", "house": "Gryffindor", "patronus": "Stag"}, 
+    {"name":"Ron", "house": "Gryffindor", "patronus": "jack Russel Terrier"}, 
+    {"name":"Darco", "house": "Slytherin", "patronus": None}, 
+]
+
+for student in students:
+    print(student["name"])   
+
+    
