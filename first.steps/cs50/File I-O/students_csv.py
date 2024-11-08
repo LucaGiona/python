43223303f940f7cdddef94b1077a032b698811b1
LucaGiona <luca@senzproduction.com>
2024-11-08 11:24:01 +0100

    adding csv exercise

--- first.steps/cs50/File I-O/students_csv.py
+++ first.steps/cs50/File I-O/students_csv.py
@@ -5,9 +5,9 @@ students = []
 with open("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
+        students.append({"uhu": row["name"], "home": row["home"], "house": row["house"], "year": row["year"]})
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+for student in sorted(students, key=lambda student: student["uhu"]):
+    print(f"{student['uhu']} is from {student['home']}. The house is {student["house"]}, built in the {student["year"]}")
 
 
