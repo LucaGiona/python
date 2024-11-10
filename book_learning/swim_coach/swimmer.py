963b4820bb7a468b884fcb9c630cef38f364f0d0
LucaGiona <luca@senzproduction.com>
2024-11-10 15:54:47 +0100

    putting times,average-time & Swimmer data in one file(Times.ipynb - swimmer.py

--- book_learning/swim_coach/swimmer.py
+++ book_learning/swim_coach/swimmer.py
@@ -1,9 +1,35 @@
-fn = "Darius-13-100m-Fly.txt"
+import statistics
 
-swimmer, age, distance, stroke = fn.removesuffix('.txt').split("-")
+FN = "Darius-13-100m-Fly.txt"
+FOLDER = "Data-from-Coach/download-swim-data/swimdata/"
 
-print(swimmer)
-print(age)
-print(distance)
-print(stroke)
+with open(FOLDER + FN) as file:
+    lines = file.readlines()
+
+swimmer, age, distance, stroke = FN.removesuffix('.txt').split("-")
+
+times= lines[0].strip().split(",")
+first = times[0]
+minutes, rest = first.split(":")
+seconds, hunderths = rest.split(".")
+converted_time = (int(minutes) *60*100) + (int(seconds)* 100) + int(hunderths)
+
+converts = []
+for t in times:
+    minutes, rest = t.split(":")
+    seconds, hunderths = rest.split(".")
+    converts.append(converted_time)
+
+average_hs = statistics.mean(converts)
+min_secs, hunderths = str(round(average_hs / 100 ,2)).split(".")
+minutes = int(min_secs) // 60
+seconds = int(min_secs) - minutes*60
+average = str(minutes) + ":" + str(seconds) + "." + hunderths
+
+print("Name: ",swimmer)
+print("Age: ", age)
+print("Distance: ", distance)
+print("Stroke: " ,stroke)
+print("All Times:",times)
+print("Average", average)
 
