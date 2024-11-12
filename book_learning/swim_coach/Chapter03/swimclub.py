813345193764fcb0a0f08b96740386811c655acb
LucaGiona <luca@senzproduction.com>
2024-11-12 21:27:32 +0100

    modified average variable :.f

--- book_learning/swim_coach/Chapter03/swimclub.py
+++ book_learning/swim_coach/Chapter03/swimclub.py
@@ -1,5 +1,8 @@
 import statistics
-FOLDER="../download-swim-data/swimdata/"
+import hfpy_utils
+
+CHARTS = "charts/"
+FOLDER = "../download-swim-data/swimdata/"
 
 def read_swim_data(filename):
     '''
@@ -26,12 +29,61 @@ def read_swim_data(filename):
         converted_time = (int(minutes) *60*100) + (int(seconds)* 100) + int(hunderths)
         converts.append(converted_time)
 
-    average_hs = statistics.mean(converts)
-    min_secs, hunderths = str(round(average_hs / 100 ,2)).split(".")
+    average = statistics.mean(converts)
+    min_secs, hunderths = f"{(average/100):.2f}".split(".")
     minutes = int(min_secs) // 60
     seconds = int(min_secs) - minutes*60
-    average = f"{minutes}:{seconds}.{hunderths}"
+    average = f"{minutes}:{seconds:0>2}.{hunderths}"
 
     
 
-    return swimmer, age, distance, stroke, times, average, converts
+    return swimmer, age, distance, stroke, times, average, converts  #returned as a tuple
+
+def produce_bar_chart(fn):
+    """Given the name of a swimmer's file, produce a HTML/SVG-based bar chart.
+     
+    Save the chart to the CHARTS folder. Return the path to the bar chart file.
+    """
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
+    from_max = max(converts)
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+    header = f"""<!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title> {title} </title>
+                        </head>
+                        <body>
+                            <h3>{title}</h3>
+            """
+    body = ""
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
+        body = body + f"""
+                        <svg height="30" width = "400">
+                            <rect height="30" width={bar_width} style="fill:rgb(30,25,125);" />
+                        </svg> {t}<br />
+                       """
+    footer = f"""
+                <p>Average time: {average}</p>
+            </body>
+            </html>
+            """
+    page = header + body + footer
+    save_to= f"{CHARTS}{fn.removesuffix(".txt")}.html"
+    with open(save_to, "w") as sf:
+        print(page, file=sf)
+    
+    return save_to
+
+# import webbrowser
+# import os
+
+# # Beispiel-Aufruf mit Dateiname
+# fn = "Darius-13-100m-Fly.txt"
+# save_to = produce_bar_chart(fn)  # HTML-Datei erstellen
+
+# # Vollständigen Pfad erstellen und im Browser öffnen
+# full_path = os.path.realpath(save_to)
+# webbrowser.open("file://" + full_path)
