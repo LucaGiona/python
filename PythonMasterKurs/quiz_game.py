7f0448c88f8f9f81676803d0e409c509d185c0ad
LucaGiona <luca@senzproduction.com>
2025-01-23 21:06:19 +0100

    changes in what to commit

--- PythonMasterKurs/quiz_game.py
+++ PythonMasterKurs/quiz_game.py
@@ -2,12 +2,35 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play?")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let`s play")
+score = 0
 
 answer = input("What does CPU stand for?")
 
 if answer == "central proccessing unit":
     print("Correct")
+    score +=1
+else: 
+    print("incorrect")
+
+answer = input("What does CPU stand for?")
+
+if answer == "central proccessing unit":
+    print("Correct")
+    score +=1
+else: 
+    print("incorrect")
+answer = input("What does CPU stand for?")
+
+if answer == "central proccessing unit":
+    print("Correct")
+    score +=1
+else: 
+    print("incorrect")
+
+print("You got " + str(score) + " points")
+
+#prozente ausrechnen
